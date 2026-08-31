151a4e4cba0053cfb06038561e609d551556a75b
Balaji S <kingbalaji4254@gmail.com>
2026-08-31 17:05:03 +0530

    Time: 133 ms (74.51%), Space: 149.2 MB (70.43%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.cpp
@@ -1,51 +1,29 @@
 class Solution {
 public:
     vector<int> lexicographicallySmallestArray(vector<int>& nums, int limit) {
-
-        int n = nums.size();
-
         vector<pair<int,int>> v;
-
-        // Store (value, original index)
-        for (int i = 0; i < n; i++) {
-            v.push_back({nums[i], i});
-        }
-
-        // Sort by value
-        sort(v.begin(), v.end());
-
+        int n=nums.size();
         vector<int> ans(n);
-
-        int i = 0;
-
-        while (i < n) {
-
-            int j = i;
-
-            // Find the current group
-            while (j + 1 < n &&
-                   v[j + 1].first - v[j].first <= limit) {
+        for(int i=0;i<n;i++){
+            v.push_back({nums[i],i});
+        }
+        sort(v.begin(),v.end());
+        int i=0;
+        while(i<n){
+            int j=i;
+            while(j+1<n&&v[j+1].first-v[j].first<=limit){
                 j++;
             }
-
-            // Collect indices of this group
             vector<int> indices;
-
-            for (int k = i; k <= j; k++) {
+            for(int k=i;k<=j;k++){
                 indices.push_back(v[k].second);
             }
-
-            // Indices must be sorted
-            sort(indices.begin(), indices.end());
-
-            // Values are already sorted because v is sorted
-            for (int k = 0; k < indices.size(); k++) {
-                ans[indices[k]] = v[i + k].first;
+            sort(indices.begin(),indices.end());
+            for(int k=0;k<indices.size();k++){
+                ans[indices[k]]=v[i+k].first;
             }
-
-            i = j + 1;
+            i=j+1;
         }
-
         return ans;
     }
 };
